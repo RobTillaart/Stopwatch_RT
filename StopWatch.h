07b383ab7f8a6714956850518736b86477a7532f
Rob Tillaart <rob.tillaart@gmail.com>
2021-01-09 11:39:04 +0100

    Arduino ci (#5)
    * add Arduino-CI
    * add unit tests

--- StopWatch.h
+++ StopWatch.h
@@ -2,18 +2,21 @@
 //
 //    FILE: StopWatch.h
 //  AUTHOR: Rob Tillaart
-// VERSION: 0.2.2
+// VERSION: 0.3.0
 // PURPOSE: Arduino Library implementing a stopwatch including seconds, milliseconds microseconds
 // HISTORY: See StopWatch.cpp
 //     URL: https://github.com/RobTillaart/StopWatch_RT
 //          http://playground.arduino.cc/Code/StopWatchClass
 //
 
+
 #include "Arduino.h"
 
-#define STOPWATCH_LIB_VERSION (F("0.2.2"))
 
-  // note - adjust divider can fix timing inaccuracies (to some extend)
+#define STOPWATCH_LIB_VERSION (F("0.3.0"))
+
+
+// NOTE: adjust divider can fix timing inaccuracies (to some extend)
 #ifndef STOPWATCH_SECONDS_DIVIDER
 #define STOPWATCH_SECONDS_DIVIDER 1000
 #endif
@@ -22,6 +25,7 @@
 #define STOPWATCH_MINUTES_DIVIDER 60000
 #endif
 
+
 class StopWatch
 {
 public:
@@ -29,19 +33,19 @@ public:
   enum Resolution { MICROS, MILLIS, SECONDS, MINUTES};
 
   explicit StopWatch(const enum Resolution res = MILLIS);
-  void start();
-  void stop();
-  void reset();
-  uint32_t elapsed();
+  void     start();
+  void     stop();
+  void     reset();
+  uint32_t elapsed();    // replaced value()
 
-  bool isRunning() { return _state == StopWatch::RUNNING; };
-  enum State state() { return _state; };
+  bool     isRunning() { return _state == StopWatch::RUNNING; };
+  enum     State state() { return _state; };
  
-  void setResolution(const enum Resolution res);
-  enum Resolution resolution() { return _res; };
+  void     setResolution(const enum Resolution res);
+  enum     Resolution resolution() { return _res; };
 
-  // value will become obsolete in the future, elapsed() is more descriptive
-  uint32_t value() { return elapsed(); };
+  // value is obsolete => elapsed() is more descriptive
+  // uint32_t value() { return elapsed(); };
 
 private:
   enum State _state;
---
+++ test/unit_test_001.cpp
@@ -0,0 +1,115 @@
+//
+//    FILE: unit_test_001.cpp
+//  AUTHOR: Rob Tillaart
+//    DATE: 2021-01-08
+// PURPOSE: unit tests for the StopWatch library
+//          https://github.com/RobTillaart/StopWatch_RT
+//          https://github.com/Arduino-CI/arduino_ci/blob/master/REFERENCE.md
+//
+
+// supported assertions
+// ----------------------------
+// assertEqual(expected, actual);               // a == b
+// assertNotEqual(unwanted, actual);            // a != b
+// assertComparativeEquivalent(expected, actual);    // abs(a - b) == 0 or (!(a > b) && !(a < b))
+// assertComparativeNotEquivalent(unwanted, actual); // abs(a - b) > 0  or ((a > b) || (a < b))
+// assertLess(upperBound, actual);              // a < b
+// assertMore(lowerBound, actual);              // a > b
+// assertLessOrEqual(upperBound, actual);       // a <= b
+// assertMoreOrEqual(lowerBound, actual);       // a >= b
+// assertTrue(actual);
+// assertFalse(actual);
+// assertNull(actual);
+
+// // special cases for floats
+// assertEqualFloat(expected, actual, epsilon);    // fabs(a - b) <= epsilon
+// assertNotEqualFloat(unwanted, actual, epsilon); // fabs(a - b) >= epsilon
+// assertInfinity(actual);                         // isinf(a)
+// assertNotInfinity(actual);                      // !isinf(a)
+// assertNAN(arg);                                 // isnan(a)
+// assertNotNAN(arg);                              // !isnan(a)
+
+#include <ArduinoUnitTests.h>
+
+
+#include "Arduino.h"
+#include "StopWatch.h"
+
+
+unittest_setup()
+{
+}
+
+unittest_teardown()
+{
+}
+
+
+unittest(test_constructor)
+{
+  fprintf(stderr, "VERSION: %s\n", STOPWATCH_LIB_VERSION);
+
+  StopWatch stopwatch0;
+
+  assertFalse(stopwatch0.isRunning());
+  assertEqual(StopWatch::RESET, stopwatch0.state());
+  assertEqual(StopWatch::MILLIS, stopwatch0.resolution());
+
+  StopWatch stopwatch1(StopWatch::MICROS);
+  assertEqual(StopWatch::MICROS, stopwatch1.resolution());
+
+  StopWatch stopwatch2(StopWatch::SECONDS);
+  assertEqual(StopWatch::SECONDS, stopwatch2.resolution());
+  
+  StopWatch stopwatch3(StopWatch::MINUTES);
+  assertEqual(StopWatch::MINUTES, stopwatch3.resolution());
+}
+
+
+unittest(test_run)
+{
+  StopWatch sw;
+
+  sw.start();
+  delay(101);
+  assertTrue(sw.isRunning());
+  assertEqual(StopWatch::RUNNING, sw.state());
+  assertLessOrEqual(100, sw.elapsed());
+
+  sw.stop();
+  assertFalse(sw.isRunning());
+  assertEqual(StopWatch::STOPPED, sw.state());
+
+  uint32_t ti = sw.elapsed();
+  sw.start();
+  delay(10);
+  assertTrue(sw.isRunning());
+  assertLessOrEqual(110, sw.elapsed());
+
+  sw.reset();
+  assertFalse(sw.isRunning());
+  assertEqual(StopWatch::RESET, sw.state());
+}
+
+
+unittest(test_resolution)
+{
+  StopWatch sw;
+
+  sw.setResolution(StopWatch::MICROS);
+  assertEqual(StopWatch::MICROS, sw.resolution());
+
+  sw.setResolution(StopWatch::MILLIS);
+  assertEqual(StopWatch::MILLIS, sw.resolution());
+
+  sw.setResolution(StopWatch::SECONDS);
+  assertEqual(StopWatch::SECONDS, sw.resolution());
+
+  sw.setResolution(StopWatch::MINUTES);
+  assertEqual(StopWatch::MINUTES, sw.resolution());
+}
+
+
+unittest_main()
+
+// --------
